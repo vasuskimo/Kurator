0912e20aaa0eaa8aea438473660b9ebcd625f18e
Vasu Srinivasan <vasusrini@Vasus-MBP.hsd1.ca.comcast.net>
2017-02-11 20:26:49 -0800

    fixed variable names

--- src/com/kurator/SatisfactionScore.java
+++ src/com/kurator/SatisfactionScore.java
@@ -27,19 +27,19 @@ public class SatisfactionScore {
 	public static final double WT = 1.0;
 	public static final double WR = 0.9;
 	
-	double avg_wait_time, curr_wait_time, avg_price, curr_price, avg_travel_time,
-	curr_travel_time, driverScore, waitScore, priceScore, ratingScore, 
+	double avgWaitTime, currWaitTime, avgPrice, currPrice, avgTravelTime,
+	currTravelTime, driverScore, waitScore, priceScore, ratingScore, 
 	travelTimeScore, totalScore;
 	
-	public SatisfactionScore(double avg_wait_time, double curr_wait_time,
-				 double avg_price, double curr_price, 
-				 double avg_travel_time, double curr_travel_time,
-				 double curr_driver_score) {
-		this.avg_wait_time = avg_wait_time;
-		this.curr_wait_time = curr_wait_time;
-		this.avg_price = avg_price;
-		this.avg_travel_time = avg_travel_time;
-		this.driverScore = curr_driver_score;
+	public SatisfactionScore(double avgWaitTime, double currWaitTime,
+				 double avgPrice, double currPrice, 
+				 double avgTravelTime, double currTravelTime,
+				 double currDriverScore) {
+		this.avgWaitTime = avgWaitTime;
+		this.currWaitTime = currWaitTime;
+		this.avgPrice = avgPrice;
+		this.avgTravelTime = avgTravelTime;
+		this.driverScore = currDriverScore;
 	}
 	
 	public void compute() {
@@ -52,30 +52,30 @@ public class SatisfactionScore {
 	}
 	
 	public void computeWaitingScore() {
-		if(avg_wait_time >= curr_wait_time) {
+		if(avgWaitTime >= currWaitTime) {
 			waitScore = 100.0;
 		} 
 		else {
-			waitScore = 100.0 - (curr_wait_time - avg_wait_time);
+			waitScore = 100.0 - (currWaitTime - avgWaitTime);
 		}
 			
 	}
 	
 	public void computePriceScore() {
-		if(avg_price >= curr_price) {
+		if(avgPrice >= currPrice) {
 			priceScore = 100.0;
 		} else {
-			priceScore = 100.0 - (curr_price - avg_price);
+			priceScore = 100.0 - (currPrice - avgPrice);
 		}
 			
 	}
 	
 	public void computeTravelTimeScore() {
-		if(avg_travel_time >= curr_travel_time) {
+		if(avgTravelTime >= currTravelTime) {
 			travelTimeScore = 100.0;
 		} else {
 			travelTimeScore = 100.0 - 
-			(curr_travel_time - avg_travel_time);
+			(currTravelTime - avgTravelTime);
 		}
 			
 	}
